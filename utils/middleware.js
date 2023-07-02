3116acb66d779b8abbb2a6c56b80a084da0c840c
JoeyG214 <erjo.guce214@gmail.com>
2023-07-02 16:28:33 -0400

    Update error handler to cover authentication errors

--- utils/middleware.js
+++ utils/middleware.js
@@ -3,21 +3,21 @@ const unknownEndpoint = (req, res) => {
 }
 
 const errorHandler = (error, req, res, next) => {
-
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
+    return res.status(400).send({ error: 'Malformatted ID' })
   } else if (error.name === 'ValidationError') { // Mongoose Schema Validation
     return res.status(400).json({ error: 'Validation Error' })
   } else if (error.name === 'JsonWebTokenError') {
-    return res.status(401).json({ error: 'invalid token' })
+    return res.status(401).json({ error: 'Invalid Token' })
   } else if (error.name === 'TokenExpiredError') {
-    return res.status(401).json({ error: 'token expired' })
+    return res.status(401).json({ error: 'Token Expired' })
+  } else if (error.message === 'invalid username') {
+    return res.status(400).json({ error: 'Invalid username' })
+  } else if (error.message === 'invalid password') {
+    return res.status(400).json({ error: 'Invalid password' })
+  } else {
+    return res.status(500).json({ error: 'Something went wrong' })
   }
-  // else {
-  //   return res.status(500).json({ error: 'Something went wrong' })
-  // }
-
-  next(error)
 }
 
 module.exports = {
