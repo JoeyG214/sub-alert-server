1ad9a9d366aeb019e21a61896978c5746105c67d
JoeyG214 <erjo.guce214@gmail.com>
2023-07-02 16:46:18 -0400

    Add route to create user

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
@@ -7,6 +8,34 @@ usersRouter.get('/', async (req, res) => {
     .populate('subscriptions', { name: 1, price: 1, billingPeriod: 1 })
 
   res.json(users)
+})
+
+usersRouter.post('/', async (req, res) => {
+  const { username, password } = req.body
+
+  if (!username || username.length < 5) {
+    return res.status(400).json({ error: 'Must provided username with atleast 5 characters.' })
+  }
+  if (!password || password.length < 5) {
+    return res.status(400).json({ error: 'Must provide password with atleast 5 characters.' })
+  }
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return res.status(400).json({ error: 'username must be unique' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  res.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
