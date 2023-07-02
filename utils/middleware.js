50f8710df93bac9b67516a240a80109fad7c6bcb
JoeyG214 <erjo.guce214@gmail.com>
2023-07-02 17:11:30 -0400

    Add error handlers

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,10 +1,10 @@
-const unknownEndpoint = (req, res) => {
+const unknownEndpoint = (_req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (error, _req, res, next) => {
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'Malformatted ID' })
+    return res.status(400).json({ error: 'Malformatted ID' })
   } else if (error.name === 'ValidationError') { // Mongoose Schema Validation
     return res.status(400).json({ error: 'Validation Error' })
   } else if (error.name === 'JsonWebTokenError') {
@@ -15,6 +15,12 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({ error: 'Invalid username' })
   } else if (error.message === 'invalid password') {
     return res.status(400).json({ error: 'Invalid password' })
+  } else if (error.message === 'Must provide username with atleast 5 characters') {
+    return res.status(400).json({ error: 'Username must be minimum 5 characters' })
+  } else if (error.message === 'Must provide password with atleast 5 characters') {
+    return res.status(400).json({ error: 'Passoword must be minimum 5 characters' })
+  } else if (error.message === 'Username must be unique') {
+    return res.status(400).json({ error: 'Username already taken' })
   } else {
     return res.status(500).json({ error: 'Something went wrong' })
   }
