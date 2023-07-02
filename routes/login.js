6720ca425885eb0ed0fca6d76ac5fb49883fe92b
JoeyG214 <erjo.guce214@gmail.com>
2023-07-02 16:12:45 -0400

    Extraced login logic to service

--- routes/login.js
+++ routes/login.js
@@ -1,38 +1,13 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
-const User = require('../models/user')
-const config = require('../utils/config')
+const authService = require('../services/loginService')
 
-loginRouter.post('/', async (req, res) => {
-  const { username, password } = req.body
-
-  const user = await User.findOne({ username })
-
-  let passwordCorrect = false
-  if (user !== null) {
-    try {
-      passwordCorrect = await bcrypt.compare(password, user.passwordHash)
-    } catch (e) {
-      return res.status(500).json({ error: 'Could not verify password' })
-    }
+loginRouter.post('/', async (request, response) => {
+  try {
+    const result = await authService.authenticate(request.body)
+    response.status(200).send(result)
+  } catch (error) {
+    response.status(403).json({ error: error.message })
   }
-
-  if (!(user && passwordCorrect)) {
-    return res.status(403).json({
-      error: user ? 'Invalid password' : 'Invalid username',
-    })
-  }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  }
-
-  // Token expires in 60 minutes
-  const token = jwt.sign(userForToken, config.SECRET, { expiresIn: 60 * 60 })
-
-  res.status(200).send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
---
+++ services/loginService.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const config = require('../utils/config')
+
+const authenticate = async ({ username, password }) => {
+  const user = await User.findOne({ username })
+
+  let passwordCorrect = false
+  if (user !== null) {
+    try {
+      passwordCorrect = await bcrypt.compare(password, user.passwordHash)
+    } catch (e) {
+      throw new Error('bcrypt error')
+    }
+  }
+
+  if (!(user && passwordCorrect)) {
+    throw new Error(user ? 'invalid password' : 'invalid username')
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  // Token expires in 60 minutes
+  const token = jwt.sign(userForToken, config.SECRET, { expiresIn: 60 * 60 })
+
+  return { token, username: user.username, name: user.name }
+}
+
+module.exports = { authenticate }
