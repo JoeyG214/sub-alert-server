28d63256b4e480bf09d606ee54d827a02f4c80f8
JoeyG214 <erjo.guce214@gmail.com>
2023-07-03 16:43:02 -0400

    Add route to delete subscriptions

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -22,7 +22,7 @@ subscriptionsRouter.post('/', userExtractor, async (req, res) => {
   }
 
   if (!name || !price || !billingPeriod) {
-    throw new Error('Blog post is missing data')
+    throw new Error('Subscription information is missing data')
   }
 
   const subscription = new Subscription({
@@ -37,4 +37,17 @@ subscriptionsRouter.post('/', userExtractor, async (req, res) => {
   await user.save()
 
   res.status(201).json(savedSubscription)
+})
+
+subscriptionsRouter.delete('/:id', userExtractor, async (req, res) => {
+  const token = req.token
+  const user = req.user
+  const { id } = req.params
+
+  if (!user || !token) {
+    throw new Error('Token is missing or invalid')
+  }
+
+  await Subscription.findByIdAndDelete(id)
+  res.status(204).end()
 })
