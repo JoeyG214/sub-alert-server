84ecc6ae893a2c175590c0a550de8a6a477e6048
JoeyG214 <erjo.guce214@gmail.com>
2023-07-03 16:37:07 -0400

    Add route to create subscription

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -11,3 +11,30 @@ subscriptionsRouter.get('/', userExtractor, async (req, res) => {
 
   res.status(200).json(subscriptions)
 })
+
+subscriptionsRouter.post('/', userExtractor, async (req, res) => {
+  const token = req.token
+  const user = req.user
+  const { name, price, billingPeriod } = req.body
+
+  if (!user || !token) {
+    throw new Error('Token is missing or invalid')
+  }
+
+  if (!name || !price || !billingPeriod) {
+    throw new Error('Blog post is missing data')
+  }
+
+  const subscription = new Subscription({
+    name,
+    price,
+    billingPeriod,
+    user: user.id,
+  })
+
+  const savedSubscription = await subscription.save()
+  user.subscriptions = user.subscriptions.concat(savedSubscription.id)
+  await user.save()
+
+  res.status(201).json(savedSubscription)
+})
